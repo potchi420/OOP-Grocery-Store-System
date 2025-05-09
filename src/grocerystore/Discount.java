1aef4a15fbd94ed494fbde0515a0930566c387b1
Franz Mesina <138673272+potchi420@users.noreply.github.com>
2025-05-09 21:06:41 +0800

    added the discount tier systen

--- src/grocerystore/Discount.java
+++ src/grocerystore/Discount.java
@@ -5,4 +5,18 @@ public class Discount {
         double discountAttained = 0.20 * price;
         return price - discountAttained;
     }
+
+    public double bronzeDiscount (double price) {
+        double discountAttained = 0.05 * price;
+        return price - discountAttained;
+    }
+    public double silverDiscount (double price) {
+        double discountAttained = 0.10 * price;
+        return price - discountAttained;
+    }
+
+    public double goldDiscount (double price) {
+        double discountAttained = 0.15 * price;
+        return price - discountAttained;
+    }
 }
