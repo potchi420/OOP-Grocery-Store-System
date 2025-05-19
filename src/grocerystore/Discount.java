0b84daaabb977aa0090b277cabc69a78f6edd409
RaplePoli <raphael12poligratis@gmail.com>
2025-05-19 11:40:58 +0800

    Update Discount.java
    With polymorphism

--- src/grocerystore/Discount.java
+++ src/grocerystore/Discount.java
@@ -1,22 +1,55 @@
 package grocerystore;
 
-public class Discount {
-    public double pwd_senior_discount (double price) {
-        double discountAttained = 0.20 * price;
-        return discountAttained;
+public abstract class Discount {
+    protected double price;
+
+    public abstract double getDiscountPrice();
+
+    public Discount(double price) {
+        this.price = price;
+    }
+}
+
+class PWD extends Discount {
+    public PWD(double price){
+        super(price);
     }
 
-    public double bronzeDiscount (double price) {
-        double discountAttained = 0.05 * price;
-        return discountAttained;
+    @Override
+    public double getDiscountPrice() {
+        return 0.20 * price;
     }
-    public double silverDiscount (double price) {
-        double discountAttained = 0.10 * price;
-        return discountAttained;
+}
+
+class Bronze extends Discount {
+    public Bronze(double price){
+        super(price);
     }
 
-    public double goldDiscount (double price) {
-        double discountAttained = 0.15 * price;
-        return discountAttained;
+    @Override
+    public double getDiscountPrice() {
+        return 0.05 * price;
+    }
+}
+
+class Silver extends Discount {
+    public Silver(double price) {
+        super(price);
+    }
+
+    @Override
+    public double getDiscountPrice() {
+        return 0.10 * price;
+    }
+}
+
+class Gold extends Discount {
+    public Gold(double price){
+        super(price);
+    }
+
+    @Override
+    public double getDiscountPrice() {
+        return 0.05 * price;
     }
 }
